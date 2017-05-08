d3ffc062151557208ef8ad27fab271f76d395d4b
Monsieur <Monsieur>
2017-05-08 16:27:00 +0800

    增加archive和标签分类

--- my_blog/urls.py
+++ my_blog/urls.py
@@ -22,6 +22,9 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', views.home , name = 'home'),
     url(r'^(?P<id>\d+)/$', views.detail, name='detail'),
+    url(r'^archives/$', views.archives, name = 'archives'),
+    url(r'^aboutme/$', views.about_me, name = 'about_me'),
+    url(r'^tag(?P<tag>\w+)/$',views.search_tag, name = 'search_tag'),
     url(r'^sj', views.sj),
     url(r'^index/$', views.index),
 
